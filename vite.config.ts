d6312cfc170d1f01a9cb92be2546949edea566e5
ween <228574227@qq.com>
2023-12-11 16:34:25 +0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,16 +15,16 @@ export default defineConfig(({ mode }) => {
     esbuild: {
       pure: ['console.log'] // 去除console.log
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          charset: false,
-          //引入variables.module.scss、mixin.scss，在vue组件和其他scss文件中可直接使用，无需再次引用，再次引用会报错
-          additionalData: `
-          @use "@/style/variables.module.scss" as *; 
-          `
-        }
-      }
-    }
+    // css: {
+    //   preprocessorOptions: {
+    //     scss: {
+    //       charset: false,
+    //       //引入variables.module.scss、mixin.scss，在vue组件和其他scss文件中可直接使用，无需再次引用，再次引用会报错
+    //       additionalData: `
+    //       @use "@/style/variables.module.scss" as *;
+    //       `
+    //     }
+    //   }
+    // }
   }
 })
